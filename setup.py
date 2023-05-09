540f81694d58a531552bfd5c0db753c8e347183c
rtdickerson <russell.t.dickerson@gmail.com>
2023-05-09 10:23:52 -0500

    added url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='requestswrapper',
       description='A plugin to wrapper the python requests library and make it easier to implement.',
       author='Russell Dickerson',
       author_email='russell.t.dickerson@gmail.com',
+      url="https://github.com/rtdickerson/requestswrapper/",
       license='MIT',
       packages=['hello'],
       zip_safe=False)
