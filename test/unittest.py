d208e476cf287e876f7736be3bfca85b46772817
rtdickerson <russell.t.dickerson@gmail.com>
2023-05-09 16:06:11 -0500

    rewrote test module

--- test/unittest.py
+++ test/unittest.py
@@ -1,144 +1,385 @@
-from lzma import MODE_FAST
 import os
 import sys
+import importlib, inspect
 
-from RequestsWrapper import RequestsWrapper
+from requestswrapper import RequestsWrapper
 
 import argparse
 import prettyprinter
 
+class test01:
+	def __init__(self):
+		self.desc = "Test #1 - GET"
+
+	def execute(self, VERBOSE=False):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeGet("https://httpbin.org/")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False:
+			print ("Error: Request unsuccessful")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		if response['result']['text'] == '' or \
+			response['result']['text'].find('SwaggerUIBundle.plugins.DownloadUrl') < 0 or \
+			response['result']['result'].find('SwaggerUIBundle.plugins.DownloadUrl') < 0 or \
+			response['result']['status_code'] != 200:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test02:
+	def __init__(self):
+		self.desc = "Test #2 - GET with Args"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeGet("https://httpbin.org/", {'arg1' : 202, 'arg2' : 'foo'})
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False:
+			print ("Error: Request unsuccessful")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		if response['result']['text'] == '' or \
+			response['result']['text'].find('SwaggerUIBundle.plugins.DownloadUrl') < 0 or \
+			response['result']['result'].find('SwaggerUIBundle.plugins.DownloadUrl') < 0 or \
+			response['result']['status_code'] != 200:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test03:
+	def __init__(self):
+		self.desc = "Test #3 - POST with JSON"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePost("https://httpbin.org/post", RO.MODE_JSON, {'arg1' : 202, 'arg2' : 'foo'})
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False or \
+			type(response['result']['result']) != type({}) or \
+			response['result']['result']['url'] != 'https://httpbin.org/post':
+				print ("Error: Test Failure")
+				prettyprinter.pprint(response)
+				return "FAIL"
+		return "OK"
+
+class test04:
+	def __init__(self):
+		self.desc = "Test #4 - POST with DATA"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePost("https://httpbin.org/post", RO.MODE_DATA, {'arg1' : 202, 'arg2' : 'foo'})
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False or \
+			type(response['result']['result']) != type('') or \
+			response['result']['result'].find('https://httpbin.org/post') < 0:
+				print ("Error: Test Failure")
+				prettyprinter.pprint(response)
+				return "FAIL"
+		return "OK"
+
+class test05:
+	def __init__(self):
+		self.desc = "Test #5 - POST a File"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		FH = open("testdata.dat", "rb")
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePost("https://httpbin.org/post", RO.MODE_FILES, {'upload_file' : FH})
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test06:
+	def __init__(self):
+		self.desc = "Test #6 - HEAD"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeHead("https://httpbin.org/")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['result']['page_size'] == 0:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test07:
+	def __init__(self):
+		self.desc = "Test #7 - HEAD with Bad URL"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeHead("https://httpbin.org/badurlnohead")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == True:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test08:
+	def __init__(self):
+		self.desc = "Test #8 - OPTIONS"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeOptions("https://httpbin.org/")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False or \
+			set(response['result']['allowed_verbs']) != set(['GET', 'OPTIONS', 'HEAD']) or \
+			set(response['result']['allowed_methods']) != set(['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']):
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test09:
+	def __init__(self):
+		self.desc = "Test #9 - GET Common Errors"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		for CODE in [404, 403, 500]:
+			if VERBOSE:
+				print ("Get a %d code" % CODE)
+			response = RO.executeGet("https://httpbin.org/status/%d" % CODE)
+			if VERBOSE:
+				print ("Response from library:")
+				print ("%s" % '-' * 60)
+				prettyprinter.pprint(response)
+				print ("%s" % '-' * 60)
+			if response['success'] == True:
+				print ("Error: Test Failure")
+				prettyprinter.pprint(response)
+				return "FAIL"
+		return "OK"
+
+class test10:
+	def __init__(self):
+		self.desc = "Test #10 - POST Common Errors"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		for CODE in [404, 403, 500]:
+			if VERBOSE:
+				print ("Get a %d code" % CODE)
+			response = RO.executePost("https://httpbin.org/status/%d" % CODE, RO.MODE_JSON)
+			if VERBOSE:
+				print ("Response from library:")
+				print ("%s" % '-' * 60)
+				prettyprinter.pprint(response)
+				print ("%s" % '-' * 60)
+			if response['success'] == True:
+				print ("Error: Test Failure")
+				prettyprinter.pprint(response)
+				return "FAIL"
+		return "OK"
+
+class test11:
+	def __init__(self):
+		self.desc = "Test #11 - DELETE"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeDelete("https://httpbin.org/delete")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test12:
+	def __init__(self):
+		self.desc = "Test #12 - DELETE with Bad URL"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executeDelete("https://httpbin.org/deletebadurl")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == True:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test13:
+	def __init__(self):
+		self.desc = "Test #13 - PATCH"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePatch("https://httpbin.org/patch")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test14:
+	def __init__(self):
+		self.desc = "Test #14 - PATCH with Bad URL"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePatch("https://httpbin.org/patchbadurl")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == True:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test15:
+	def __init__(self):
+		self.desc = "Test #15 - PUT"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePut("https://httpbin.org/put")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == False:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+class test16:
+	def __init__(self):
+		self.desc = "Test #16 - PUT with Bad URL"
+
+	def execute(self, VERBOSE):
+		print ("Begin %s" % self.desc)
+		RO = RequestsWrapper.RequestWrapper()
+		response = RO.executePut("https://httpbin.org/putbadurl")
+		if VERBOSE:
+			print ("Response from library:")
+			print ("%s" % '-' * 60)
+			prettyprinter.pprint(response)
+			print ("%s" % '-' * 60)
+		if response['success'] == True:
+			print ("Error: Test Failure")
+			prettyprinter.pprint(response)
+			return "FAIL"
+		return "OK"
+
+
+TEST_TOC = {}
+for name, cls in inspect.getmembers(importlib.import_module(__name__), inspect.isclass):
+	TEST_TOC[name] = {}
+	OBJ = globals()[name]()
+	TEST_TOC[name]['desc'] = OBJ.desc
+	TEST_TOC[name]['obj'] = OBJ
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--test1', help="execute test #1 - GET", action="store_true")
-parser.add_argument('--test2', help="execute test #2 - GET with Args", action="store_true")
-parser.add_argument('--test3', help="execute test #3 - POST JSON", action="store_true")
-parser.add_argument('--test4', help="execute test #4 - POST DATA", action="store_true")
-parser.add_argument('--test5', help="execute test #5 - POST File", action="store_true")
-parser.add_argument('--test6', help="execute test #6 - HEAD", action="store_true")
-parser.add_argument('--test7', help="execute test #7 - HEAD Bad", action="store_true")
-parser.add_argument('--test8', help="execute test #8 - OPTIONS", action="store_true")
-parser.add_argument('--test9', help="execute test #9 - GET Common Errors", action="store_true")
-parser.add_argument('--test10', help="execute test #10 - POST Common Errors", action="store_true")
-parser.add_argument('--test11', help="execute test #11 - DELETE", action="store_true")
-parser.add_argument('--test12', help="execute test #12 - DELETE BAD", action="store_true")
-parser.add_argument('--test13', help="execute test #13 - PATCH", action="store_true")
-parser.add_argument('--test14', help="execute test #14 - PATCH BAD", action="store_true")
-parser.add_argument('--test15', help="execute test #15 - PUT", action="store_true")
-parser.add_argument('--test16', help="execute test #16 - PUT BAD", action="store_true")
+for TEST in TEST_TOC.keys():
+	parser.add_argument('--%s' % TEST, help=TEST_TOC[TEST]['desc'], action="store_true")
+parser.add_argument('--all', help="ALL Tests", action="store_true")
+parser.add_argument('--verbose', help="Verbose mode", action="store_true")
 args = parser.parse_args()
+ARGS = vars(args)
 
-if args.test1:
-	print ("Test 1")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeGet("https://httpbin.org/")
-	#prettyprinter.pprint(response)
-	assert(response['success'] == True)
-	assert(response['result']['text'] != '')
-	assert(response['result']['text'].find('SwaggerUIBundle.plugins.DownloadUrl') > 0)
-	assert(response['result']['result'].find('SwaggerUIBundle.plugins.DownloadUrl') > 0)
-	assert(response['result']['status_code'] == 200)
-elif args.test2:
-	print ("Test 2")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeGet("https://httpbin.org/", {'arg1' : 202, 'arg2' : 'foo'})
-	#prettyprinter.pprint(response)
-	assert(response['success'] == True)
-	assert(response['result']['text'] != '')
-	assert(response['result']['text'].find('SwaggerUIBundle.plugins.DownloadUrl') > 0)
-	assert(response['result']['result'].find('SwaggerUIBundle.plugins.DownloadUrl') > 0)
-	assert(response['result']['status_code'] == 200)
-elif args.test3:
-	print ("Test 3")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePost("https://httpbin.org/post", RO.MODE_JSON, {'arg1' : 202, 'arg2' : 'foo'})
-	#prettyprinter.pprint(response)
-	assert(response['success'] == True)
-	assert(type(response['result']['result']) == type({}))
-	assert(response['result']['result']['url'] == 'https://httpbin.org/post')
-	#prettyprinter.pprint (response['result']['result'])
-elif args.test4:
-	print ("Test 4")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePost("https://httpbin.org/post", RO.MODE_DATA, {'arg1' : 202, 'arg2' : 'foo'})
-	assert(response['success'] == True)
-	assert(type(response['result']['result']) == type(''))
-	#prettyprinter.pprint(response)
-	assert(response['result']['result'].find('https://httpbin.org/post') > 0)
-	#prettyprinter.pprint (response['result']['result'])
-elif args.test5:
-	print ("Test 5")
-	FH = open("testdata.dat", "rb")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePost("https://httpbin.org/post", RO.MODE_FILES, {'upload_file' : FH})
-	prettyprinter.pprint(response)
-elif args.test6:
-	print ("Test 6")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeHead("https://httpbin.org/")
-	#prettyprinter.pprint(response)
-	assert(response['result']['page_size'] > 0)
-elif args.test7:
-	print ("Test 7")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeHead("https://httpbin.org/badurlnohead")
-	prettyprinter.pprint(response)
-	assert(response['success'] == False)
-elif args.test8:
-	print ("Test 8")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeOptions("https://httpbin.org/")
-	prettyprinter.pprint(response)
-	assert(response['success'] == True)
-	assert(set(response['result']['allowed_verbs']) == set(['GET', 'OPTIONS', 'HEAD']))
-	assert(set(response['result']['allowed_methods']) == set(['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']))
-	
-elif args.test9:
-	print ("Test 9")
-	RO = RequestsWrapper.RequestWrapper()
-	for CODE in [404, 403, 500]:
-		response = RO.executeGet("https://httpbin.org/status/%d" % CODE)
-		assert(response['success'] == False)
-elif args.test10:
-	print ("Test 10")
-	RO = RequestsWrapper.RequestWrapper()
-	for CODE in [404, 403, 500]:
-		response = RO.executePost("https://httpbin.org/status/%d" % CODE, RO.MODE_JSON)
-		assert(response['success'] == False)
-elif args.test11:
-	print ("Test 11")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeDelete("https://httpbin.org/delete")
-	prettyprinter.pprint(response)
-	assert(response['success'] == True)
-elif args.test12:
-	print ("Test 12")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executeDelete("https://httpbin.org/deletebadurl")
-	prettyprinter.pprint(response)
-	assert(response['success'] == False)
-elif args.test13:
-	print ("Test 13")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePatch("https://httpbin.org/patch")
-	prettyprinter.pprint(response)
-	assert(response['success'] == True)
-elif args.test14:
-	print ("Test 14")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePatch("https://httpbin.org/patchbadurl")
-	prettyprinter.pprint(response)
-	assert(response['success'] == False)
-elif args.test15:
-	print ("Test 15")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePut("https://httpbin.org/put")
-	prettyprinter.pprint(response)
-	assert(response['success'] == True)
-elif args.test16:
-	print ("Test 16")
-	RO = RequestsWrapper.RequestWrapper()
-	response = RO.executePut("https://httpbin.org/putbadurl")
-	prettyprinter.pprint(response)
-	assert(response['success'] == False)
-else:
-	print ("No test specified")
+for TEST in TEST_TOC.keys():
+	if ARGS[TEST] == True:
+		result = TEST_TOC[TEST]['obj'].execute(args.verbose)
+		if result == "OK":
+			sys.exit(0)
+		else:
+			sys.exit(1)
+
+if args.all:
+	CNT = 0
+	ERR = 0
+	for TEST in TEST_TOC.keys():
+		CNT += 1
+		result = TEST_TOC[TEST]['obj'].execute(args.verbose)
+		if result == "OK":
+			continue
+		else:
+			ERR += 1
+	if ERR > 0:
+		print ("%d test of %d had an error result!" % (ERR, CNT))
+		sys.exit(1)
+	else:
+		print ("%d tests run successfully" % CNT)
+		sys.exit(0)
+
+print ("No tests selected!")
+parser.print_help(sys.stderr)
+sys.exit(1)
