69c6a0d9481a6a2bda53c9ed66589c1347f2c60c
rtdickerson <russell.t.dickerson@gmail.com>
2023-05-10 14:27:43 -0500

    Reworked exception handling

--- requestswrapper/RequestsWrapper.py
+++ requestswrapper/RequestsWrapper.py
@@ -5,9 +5,17 @@ import prettyprinter
 
 import requests 
 
-class RequestWrapper(object):
+class RequestsWrapper(object):
+	"""
+	The RequestsWrapper class is a convenience class that wrappers the python requests library.  Making calls and handling cookies, etc.
+	can require a lot of overhead and this class simplifies that.
+	The class is derived from object, so it can be subclasses to customize into different uses.
+	"""
 
 	def __init__(self, PROXIES=None):
+		'''
+		Initialization.  You can customize headers here.
+		'''
 		self.PROXIES = PROXIES
 		self.SESSION = requests.Session()
 		self.SESSION.proxies = PROXIES
@@ -29,18 +37,85 @@ class RequestWrapper(object):
 		self.RETURN_AS_JSON = False
 
 	def addHeader(self, KEY, VAL):
+		"""
+		Add a custom header to the global header list.
+
+		KEY - The Header Key like 'User-Agent'
+		VAL - The value to set.
+		"""
 		self.HEADERS[KEY] = VAL
 
 	def addCookie(self, cookiename, key, DOMAIN, PATH):
+		"""
+		Add a custom cookie
+		"""
 		self.COOKIEJAR.set(cookiename, key, DOMAIN, PATH)
 
 	def returnError(self, ERRMSG, ERRDATA=None):
+		'''
+		This convenience function ensures error responses follow the same format.
+		'''
 		return {'success' : False, 'error' : ERRMSG, 'details' : ERRDATA}
 
+	def requestExceptionHandler(self, ex, url, details):
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		# Create the basic response
+		response = {}
+		response['success'] = False
+		response['url'] = url
+		response['file'] = fname
+		response['type'] = exc_type
+		response['lineno'] = exc_tb.tb_lineno
+		response['url'] = url
+		response['details'] = details
+
+		if type(ex) == requests.ConnectionError:
+			response['error'] = "Connection error attempting GET"
+			response['request'] = ex.request
+			response['response'] = ex.response
+			return response
+		elif type(ex) == requests.exceptions.Timeout:
+			response['error'] = "Timeout error attempting GET"
+			response['request'] = ex.request
+			response['response'] = ex.response
+			return response
+		elif type(ex) == requests.exceptions.TooManyRedirects:
+			response['error'] = "Invalid URL"
+			response['request'] = ex.request
+			response['response'] = ex.response
+			return response
+		elif type(ex) == requests.ReadTimeout:
+			response['error'] = "Read Timeout"
+			response['request'] = ex.request
+			response['response'] = ex.response
+			return response
+		elif type(ex) == requests.JSONDecodeError:
+			response['error'] = "JSON Decode Error"
+			response['request'] = ex.request
+			response['response'] = ex.response
+			return response
+		elif type(ex) == requests.RequestException:
+			response['error'] = "Generic Request Exception"
+			response['request'] = ex.request
+			response['response'] = ex.response
+			return response
+		else:
+			# Unhandled exception, raise
+			raise ex
+
 	def returnSuccess(self, RETURN):
+		'''
+		This convenience function ensures success responses follow the same format.
+		'''
 		return {'success' : True, 'result' : RETURN}
 
+	
 	def executeGet(self, URL, ARGLIST=None):
+		'''
+		This function executes an HTTP GET request.  It can handle a single URL, or the URL can have an argument list passed
+		as a python DICT to append the ?key=val&key2=val2.
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -56,18 +131,8 @@ class RequestWrapper(object):
 				webresult = self.SESSION.get(URL, **OPTS)
 			else:
 				webresult = self.SESSION.get(URL, params=ARGLIST, **OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST})
 
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
@@ -90,9 +155,16 @@ class RequestWrapper(object):
 		RET['result'] = RESULT
 		return self.returnSuccess(RET)
 
-
-
 	def executePost(self, URL, MODE, POSTDATA=None):
+		'''
+		This function executes an HTTP POST request.  It can handle a single URL, or the URL can have POST data.  Given you
+		can post a JSON, or DATA, or a file, this function handles all 3.
+		
+		There are 3 modes defined in the class:
+			MODE_JSON : POSTDATA is sent using the json= keyword
+			MODE_DATA : POSTDATA is sent using the data= keyword
+			MODE_FILES : POSTDATA is sent using a DICT and the files= keyword
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -117,21 +189,13 @@ class RequestWrapper(object):
 
 		try:
 			webresult = self.SESSION.post(URL, OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting POST", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting POST", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST, 'mode' : MODE})
+
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
 					{'url' : URL, 'history' : webresult.history})
+
 		RET = {}
 		RET['headers'] = webresult.headers
 		RET['text'] = webresult.text
@@ -150,6 +214,10 @@ class RequestWrapper(object):
 
 
 	def executeHead(self, URL, ARGLIST=None):
+		'''
+		This function executes an HTTP HEAD request.  It can handle a single URL, or the URL can have an argument list passed
+		as a python DICT to append the ?key=val&key2=val2.
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -165,18 +233,9 @@ class RequestWrapper(object):
 				webresult = self.SESSION.head(URL, **OPTS)
 			else:
 				webresult = self.SESSION.head(URL, param=ARGLIST, **OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting HEAD", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting HEAD", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST})
+
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
 					{'url' : URL, 'history' : webresult.history})
@@ -189,6 +248,10 @@ class RequestWrapper(object):
 		return self.returnSuccess(RET)
 
 	def executeOptions(self, URL, ARGLIST=None):
+		'''
+		This function executes an HTTP OPTIONS request.  It can handle a single URL, or the URL can have an argument list passed
+		as a python DICT to append the ?key=val&key2=val2.
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -204,18 +267,9 @@ class RequestWrapper(object):
 				webresult = self.SESSION.options(URL, **OPTS)
 			else:
 				webresult = self.SESSION.options(URL, param=ARGLIST, **OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting OPTIONS", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting OPTIONS", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST})
+
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
 					{'url' : URL, 'history' : webresult.history})
@@ -239,7 +293,10 @@ class RequestWrapper(object):
 		return self.returnSuccess(RET)
 
 	def executeDelete(self, URL, ARGLIST=None):
-
+		'''
+		This function executes an HTTP DELETE request.  It can handle a single URL, or the URL can have an argument list passed
+		as a python DICT to append the ?key=val&key2=val2.
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -255,18 +312,9 @@ class RequestWrapper(object):
 				webresult = self.SESSION.delete(URL, **OPTS)
 			else:
 				webresult = self.SESSION.delete(URL, params=ARGLIST, **OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST})
+
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
 					{'url' : URL, 'history' : webresult.history})
@@ -284,6 +332,10 @@ class RequestWrapper(object):
 		return self.returnSuccess(RET)
 
 	def executePatch(self, URL, ARGLIST=None):
+		'''
+		This function executes an HTTP PATCH request.  It can handle a single URL, or the URL can have an argument list passed
+		as a python DICT to append the ?key=val&key2=val2.
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -299,18 +351,9 @@ class RequestWrapper(object):
 				webresult = self.SESSION.patch(URL, **OPTS)
 			else:
 				webresult = self.SESSION.patch(URL, param=ARGLIST, **OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST})
+
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
 					{'url' : URL, 'history' : webresult.history})
@@ -331,6 +374,10 @@ class RequestWrapper(object):
 		return self.returnSuccess(RET)
 
 	def executePut(self, URL, ARGLIST=None):
+		'''
+		This function executes an HTTP PUT request.  It can handle a single URL, or the URL can have an argument list passed
+		as a python DICT to append the ?key=val&key2=val2.
+		'''
 		OPTS = {
 			'allow_redirects' : self.REDIRECTS,
 			'auth' : self.AUTH,
@@ -346,21 +393,14 @@ class RequestWrapper(object):
 				webresult = self.SESSION.put(URL, **OPTS)
 			else:
 				webresult = self.SESSION.put(URL, param=ARGLIST, **OPTS)
-		except requests.ConnectionError:
-			return self.returnError("Connection error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.Timeout:
-			return self.returnError("Timeout error attempting GET", {'url' : URL, 'history' : webresult.history})
-		except requests.exceptions.TooManyRedirects:
-			return self.returnError("Invalid URL", {'url' : URL, 'history' : webresult.history})
-		except requests.ReadTimeout:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history})
-		except requests.JSONDecodeError:
-			return self.returnError("Read Timeout", {'url' : URL, 'history' : webresult.history, 'text' : webresult.text})
-		except requests.RequestException as ex:
-			return self.returnError("Requests Error %s" % ex,  {'url' : URL, 'history' : webresult.history})
+
+		except Exception as ex:
+			return self.requestExceptionHandler(ex, URL, {'arglist' : ARGLIST})
+
 		if not webresult.status_code in [200]:
 			return self.returnError("HTTP response %d - %s" % (webresult.status_code, HTTPStatus(webresult.status_code).phrase),
 					{'url' : URL, 'history' : webresult.history})
+
 		RET = {}
 		RET['headers'] = webresult.headers
 		RET['text'] = webresult.text
